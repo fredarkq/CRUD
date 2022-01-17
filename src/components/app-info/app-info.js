71841cd5e12a61101e1f84b0d1c8394386b27d18
Eugen <fredarkq18@gmail.com>
2022-01-17 19:33:13 +0200

    Connected toggle buttons in 1 ToggleProp, debugged empty adding in 'addForm'

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,12 +1,14 @@
 
 import './app-info.css'
 
-const AppInfo = () => {
+const AppInfo = (props) => {
+    const {data} = props
+    const incTotal = data.filter(item => item.increase).length
     return (
         <div className="app-info">
             <h1>Учет сотрудников в компании №</h1>
-            <h2>Общее число сотрудников: </h2>
-            <h2>Премию получат: </h2>
+            <h2>Общее число сотрудников: {data.length} </h2>
+            <h2>Премию получат:{incTotal} </h2>
         </div>
     )
 }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,9 +14,9 @@ class App extends Component {
         super(props);
         this.state = {
             data: [
-                {name: "Petyovka I.", salary:800, id: 1},
-                {name: "Baloga I.", salary:3000,  id: 2},
-                {name: "Fediv E.", salary:15000, id: 3}
+                {name: "Petyovka I.", salary:800, increase: false, rise: true, id: 1},
+                {name: "Baloga I.", salary:3000, increase: false, rise: false, id: 2},
+                {name: "Fediv E.", salary:15000, increase: false, rise: false, id: 3}
             ]
         }
         this.maxId = 4;
@@ -36,21 +36,52 @@ addItem = (name, salary) => {
     const newItem = {
         name, 
         salary,
+        increase: false,
+        rise: false,
         id: this.maxId++
-    }
+    } 
+
     this.setState(({data}) => {
         const newArr = [...data, newItem];
         return {
+            
             data: newArr
         }
     });
 }
 
+onToggleProp = (id, prop) => {
+    // this.setState(({data}) => {
+        // const index = data.findIndex(elem => elem.id === id);
+
+        // const old = data[index];
+        // const newItem = {...old, increase: !old.increase};
+        // const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+        // return{
+        //     data: newArr
+        // }
+
+
+    // })
+
+    this.setState(({data}) => ({
+        data: data.map(item => {
+            if (item.id === id) {
+                return {...item, [prop]: !item[prop] }
+            }
+            return item;
+        })
+    }))
+}
+
+
     render () {
         
         return (
             <div className="app">
-                <AppInfo/>
+                <AppInfo
+                data={this.state.data}/>
     
                 <div className="search-panel">
                     <SearchPanel/>
@@ -59,7 +90,8 @@ addItem = (name, salary) => {
     
                 <EmployeesList 
                 data={this.state.data}
-                onDelete={this.deleteItem}/>
+                onDelete={this.deleteItem}
+                onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm
                 onAdd={this.addItem}
                 />
